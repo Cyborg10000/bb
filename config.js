c19b9207f780d12646df9cf9765714a55c29e6d2
Cyborg10000 <148667338+Cyborg10000@users.noreply.github.com>
2023-10-21 22:04:28 -0500

    Update config.js

--- config.js
+++ config.js
@@ -8,10 +8,8 @@ import axios from 'axios';
 import moment from 'moment-timezone';
 
 global.owner = [
-  ['5212412377467', 'CREADOR', true],
-  ['5212411233838', 'CREADOR', true],
-  ['5212411719888', 'CREADOR', true],
-  ['5212414147874', 'CREADOR', true]
+ 
+  ['573208266162', 'CREADOR', true]
   
 ];
 
